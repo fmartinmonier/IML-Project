3b1dc6a135c01a646737aef3bb8ad64b8b0622df
Chang Mei-Chih <37042542+MeiChihChang@users.noreply.github.com>
2021-03-31 12:36:11 +0200

    Add files via upload

--- task1b_LinReg_FeatureTransformation/main_mei.py
+++ task1b_LinReg_FeatureTransformation/main_mei.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 train = pd.read_csv("train.csv" )
 
 y = train['y']
-#normalize_y = y / np.linalg.norm(y)
+normalize_y = y / np.linalg.norm(y)
 X = train [['x1', 'x2', 'x3', 'x4', 'x5']]
 size=y.size
 print(type(X))
@@ -23,16 +23,16 @@ cos = np.cos(X)
 
 finale = np.concatenate((X, square, exponential, cos, np.ones((X.shape[0],1))), axis = 1)
 print(np.shape(finale))
-#normalize1 = finale / np.linalg.norm(finale)
-#df = pd.DataFrame(normalize1)
-df = pd.DataFrame(finale)
+normalize1 = finale / np.linalg.norm(finale)
+df = pd.DataFrame(normalize1)
+#df = pd.DataFrame(finale)
 
 model = Ridge(alpha=0.025)
 #lm = LinearRegression()
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-y_pred = cross_validate(model, df, y, cv=cv, return_estimator=True)
-model.fit(finale,y)
-predictions = model.predict(finale)
+y_pred = cross_validate(model, df, normalize_y, cv=cv, return_estimator=True)
+model.fit(normalize1,normalize_y)
+predictions = model.predict(normalize1)
 
 counter = 0
 newmodel_coef_ = np.zeros((finale.shape[1],1))
@@ -42,14 +42,12 @@ for model in y_pred['estimator']:
 print(newmodel_coef_.shape)
 weights = np.mean(newmodel_coef_, axis = 0)
 
-#weights = model.coef_
-#weights = np.transpose(weights)
 weights = np.transpose(weights)
 print(weights)
 
 plt.figure(figsize=(12, 6))
 plt.plot(predictions, 'r+')
-plt.plot(y, 'bo')
+plt.plot(normalize_y, 'bo')
 plt.show()
 
 DF = pd.DataFrame(weights)
