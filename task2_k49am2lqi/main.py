a547afe04407078ceb4adca10336c03ca9a00faf
fmartinmonier <74377209+fmartinmonier@users.noreply.github.com>
2021-04-20 23:53:50 +0200

    Add files via upload
    task 3 still to do

--- task2_k49am2lqi/main.py
+++ task2_k49am2lqi/main.py
@@ -1,88 +1,72 @@
 import pandas as pd 
 from sklearn.svm import SVC, SVR
 import numpy as np
-svclassifier_task1 = SVC(kernel='sigmoid') 
-svclassifier_task2 = SVC(kernel='sigmoid')
+
+#Defining classification and regression models for tasks 1,2 and 3
+svclassifier_task1 = SVC(kernel='sigmoid', probability=True) 
+svclassifier_task2 = SVC(kernel='sigmoid', probability=True)
 svregression_task3 = SVR(kernel='sigmoid')
 
-#How to deal with nan data through pre-processing step?
+
+vitals = ['RRate', 'ABPm', 'SpO2', 'Heartrate']
+VITALS = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
+
+tests = ['BaseExcess', 'Fibrinogen', 'AST', 'Alkalinephos', 'Bilirubin_total', 'Lactate', 'TroponinI', 'SaO2', 'Bilirubin_total','EtCO2']
+TESTS = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
+         'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2',
+         'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
 
 #load dataset
-train_feature = pd.read_csv("train_features.csv" )
+train_features = pd.read_csv("train_features.csv" )
 train_labels = pd.read_csv("train_labels.csv" )
-test_feature = pd.read_csv("test_features.csv" )
+test_features = pd.read_csv("test_features.csv" )
 
-pid = train_feature['pid']
-pid_test = test_feature['pid']
-add_info = train_feature[['Time', 'Age']]
-add_info_test = test_feature[['Time', 'Age']]
+#Replacing NaN values with means of their respective columns (mean computed over all patient data)
+train_features = train_features.fillna(train_features.mean())
 
-task_1 = train_feature
-task_1_y = train_labels[['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total', 'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_total','LABEL_EtCO2']]
-task_1_test = test_feature
-
-task_2 = train_feature
-task_2_y = train_labels['LABEL_Sepsis']
-task_2_test = test_feature
-
-task_3 = train_feature
-task_3_y = train_labels[['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']]
-task_3_test = test_feature
-
-add_info = add_info.to_numpy()
-add_info_test = add_info_test.to_numpy()
-
-train_feature = train_feature.to_numpy()
-train_labels = train_labels.to_numpy()
-
-task_1 = task_1.to_numpy()
-task_1_y = task_1_y.to_numpy()
-task_1_test = task_1_test.to_numpy
-
-task_2 = task_2.to_numpy()
-task_2_y = task_2_y.to_numpy()
-task_2_test = task_2_test.to_numpy()
-
-task_3 = task_3.to_numpy()
-task_3_y = task_3_y.to_numpy()
-task_3_test = task_3_test.to_numpy()
-
-#print(train_feature)
-
-print('train_labels', np.size(train_labels,0), 'task1', np.size(task_1,0))
+pid = train_features['pid']
+pid_test = test_features['pid']
 
 pid_list = pid.to_numpy()
 pid_list = np.unique(pid_list)
 pid_list_test = pid_test.to_numpy()
 pid_list_test = np.unique(pid_list_test)
-
-#Task 1
 print('pid_list', np.size(pid_list,0))
 
-#Training
-pid_attribute_t1 = []
-pid_attribute_t2 = []
-pid_attribute_t3 = []
-i=0
+#Creating a new time collumn, with timesteps for each patient being in range(1,12)
+time = {'Time':[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+time = pd.DataFrame(time,columns=['Time'])
+time_repeated = pd.concat([time]*np.size(pid_list,0), ignore_index=True)
+train_features['Time'] = time_repeated['Time']
 
-for i in range(np.size(pid_list,0)):
-    j=0
-    for j in range(np.size(task_1,0)):
-        while pid_list[i] == train_feature[j,0]:
-            pid_attribute_t1 = np.concatenate((pid_attribute_t1, train_feature[j,:]), axis=1)
-            pid_attribute_t2 = np.concatenate((pid_attribute_t2, train_feature[j,:]), axis=1)
-            pid_attribute_t3 = np.concatenate((pid_attribute_t3, train_feature[j,:]), axis=1)
-            j = j + 1
-    print(pid_attribute_t1)
 
-    svclassifier_task1.fit(pid_attribute_t1, task_1_y[i])
-    svclassifier_task2.fit(pid_attribute_t2, task_2_y[i])
-    svregression_task3.fit(pid_attribute_t3, task_3_y[i])
-    pid_attribute_t1 = []
-    pid_attribute_t2 = []
-    pid_attribute_t3 = []
+#TRAINING
 
-#Testing
+#Task 1 --> For-loop commented out due to long execution time
+
+#Merging both training and testin datasets on the patient id number
+all_training_data = pd.merge_ordered(train_features, train_labels, on='pid')
+
+svclassifier_task1_ = {}
+
+#for label in TESTS:
+    #k = 0
+    #test_type = tests[k]
+    #print('Current test type being trained:', test_type)
+    #X_train = all_training_data.pivot_table(index="pid", columns="Time", values=test_type).to_numpy()
+    #y_train = all_training_data.pivot_table(index="pid", values=label).to_numpy()
+    #svclassifier_task1_[str(label)] = svclassifier_task1.fit(X_train, y_train.ravel())
+
+
+#Task 2
+label = ['LABEL_Sepsis']
+X_train_task2 = all_training_data.pivot_table(index="pid", columns="Time").to_numpy()
+y_train_task2 = all_training_data.pivot_table(index="pid", values=label).to_numpy()
+svclassifier_task2_sepsis = svclassifier_task2.fit(X_train_task2, y_train_task2.ravel())
+
+#Task 3 --> To do
+
+#TESTING --> To do. The following code is the old version, using numpy instead on pandas for the data manipulation.
 
 pid_attribute_t1 = []
 pid_attribute_t2 = []
@@ -94,7 +78,7 @@ for i in range(np.size(pid_list,0)):
     j=0
     for j in range(np.size(task_1,0)):
         while pid_list[i] == train_feature[j,0]:
-            pid_attribute_t1 = np.concatenate((pid_attribute_t1, test_feature[j,:]), axis=0)
+            pid_attribute_t1 = np.vstack((pid_attribute_t1, test_feature[j,:]))
             pid_attribute_t2 = np.concatenate((pid_attribute_t2, test_feature[j,:]), axis=0)
             pid_attribute_t3 = np.concatenate((pid_attribute_t3, test_feature[j,:]), axis=0)
             j = j + 1
