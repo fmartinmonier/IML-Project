c441b003364a3942201c1500726b280861bac783
Valentin <56586152+Babars7@users.noreply.github.com>
2021-04-22 14:34:14 +0200

    Add files via upload

--- task2_k49am2lqi/main.py
+++ task2_k49am2lqi/main.py
@@ -3,7 +3,7 @@ from sklearn.svm import SVC, SVR
 import numpy as np
 
 #Defining classification and regression models for tasks 1,2 and 3
-svclassifier_task1 = SVC(kernel='sigmoid', probability=True) 
+svclassifier_task1 = SVC(kernel='sigmoid') 
 svclassifier_task2 = SVC(kernel='sigmoid', probability=True)
 svregression_task3 = SVR(kernel='sigmoid')
 
@@ -11,10 +11,9 @@ svregression_task3 = SVR(kernel='sigmoid')
 vitals = ['RRate', 'ABPm', 'SpO2', 'Heartrate']
 VITALS = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
 
-tests = ['BaseExcess', 'Fibrinogen', 'AST', 'Alkalinephos', 'Bilirubin_total', 'Lactate', 'TroponinI', 'SaO2', 'Bilirubin_total','EtCO2']
-TESTS = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
-         'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2',
-         'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
+tests = ['BaseExcess', 'Fibrinogen', 'AST']#, 'Alkalinephos', 'Bilirubin_total', 'Lactate', 'TroponinI', 'SaO2', 'Bilirubin_total','EtCO2']
+TESTS = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST']#, 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total','LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2','LABEL_Bilirubin_direct', 'LABEL_EtCO2']
+head = [ 'pid','LABEL_BaseExcess','LABEL_Fibrinogen','LABEL_AST','LABEL_Alkalinephos','LABEL_Bilirubin_total','LABEL_Lactate','LABEL_TroponinI','LABEL_SaO2','LABEL_Bilirubin_direct','LABEL_EtCO2','LABEL_Sepsis','LABEL_RRate','LABEL_ABPm','LABEL_SpO2','LABEL_Heartrate']
 
 #load dataset
 train_features = pd.read_csv("train_features.csv" )
@@ -23,21 +22,24 @@ test_features = pd.read_csv("test_features.csv" )
 
 #Replacing NaN values with means of their respective columns (mean computed over all patient data)
 train_features = train_features.fillna(train_features.mean())
+test_features = test_features.fillna(test_features.mean())
 
 pid = train_features['pid']
+pid_list=pid.unique() #applying .unique() takes unique value repeated + transform in ndarray
 pid_test = test_features['pid']
+pid_list_test = pid_test.unique()
 
-pid_list = pid.to_numpy()
-pid_list = np.unique(pid_list)
-pid_list_test = pid_test.to_numpy()
-pid_list_test = np.unique(pid_list_test)
-print('pid_list', np.size(pid_list,0))
+
+
 
 #Creating a new time collumn, with timesteps for each patient being in range(1,12)
 time = {'Time':[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
 time = pd.DataFrame(time,columns=['Time'])
 time_repeated = pd.concat([time]*np.size(pid_list,0), ignore_index=True)
 train_features['Time'] = time_repeated['Time']
+
+time_repeated_test = pd.concat([time]*np.size(pid_list_test,0), ignore_index=True)
+test_features['Time'] = time_repeated_test['Time']
 
 #Merging both training and testin datasets on the patient id number
 all_training_data = pd.merge_ordered(train_features, train_labels, on='pid')
@@ -53,18 +55,48 @@ k = 0 #integer increment at each iterator of the for loop to iterate on the "tes
 for label in TESTS:
     test_type = tests[k]
     print('Current test type being trained:', test_type)
-    X_train_task1 = all_training_data.pivot_table(index="pid", columns="Time", values=test_type).to_numpy()
-    y_train_task1 = all_training_data.pivot_table(index="pid", values=label).to_numpy()
+    X_train_task1 = train_features.pivot_table(index="pid", columns="Time", values=test_type).to_numpy()
+    y_train_task1 = train_labels.pivot_table(index="pid", values=label).to_numpy()
+    print(test_type)
+    svclassifier_task1 = SVC(kernel='sigmoid') 
     svclassifier_task1_[str(test_type)] = svclassifier_task1.fit(X_train_task1, y_train_task1.ravel())
     k += 1
 
+#TESTING
+###########################################################################
+#Task1
+k = 0 
+pred_t1 = []
+result_t1 = np.expand_dims(pid_list_test, axis=1)
+for label in TESTS:
+    test_type = tests[k]
+    X_test_task1 = test_features.pivot_table(index="pid", columns="Time", values=test_type).to_numpy()
+    
+    pred_t1 = svclassifier_task1_[str(test_type)].predict(X_test_task1)
+    k += 1
+    print(pred_t1)
+    result_t1 = np.concatenate((result_t1, np.expand_dims(pred_t1, axis=1)), axis=1)
+print("result_t1", result_t1.shape)
+###########################################################################
 ######################################
 #Task 2
 label = ['LABEL_Sepsis']
-X_train_task2 = all_training_data.pivot_table(index="pid", columns="Time").to_numpy()
-y_train_task2 = all_training_data.pivot_table(index="pid", values=label).to_numpy()
+X_train_task2 = train_features.pivot_table(index="pid", columns="Time").to_numpy()
+y_train_task2 = train_labels.pivot_table(index="pid", values=label).to_numpy()
 svclassifier_task2_sepsis = svclassifier_task2.fit(X_train_task2, y_train_task2.ravel())
 
+###########################################################################
+#Task2 TESTING
+
+label = ['LABEL_Sepsis']
+X_test_task2 = test_features.pivot_table(index="pid", columns="Time").to_numpy()
+pred_t2 = svclassifier_task2_sepsis.predict(X_test_task2)
+print("pred_t2",np.expand_dims(pred_t2, axis=1).shape)
+
+result_t2 = np.concatenate((result_t1, np.expand_dims(pred_t2, axis=1)), axis=1)
+
+######################################
+print("result_t2", result_t2.shape)
 ######################################
 #Task 3 
 svregression_task3_ = {}
@@ -73,41 +105,32 @@ k = 0 #integer increment at each iterator of the for loop to iterate on the "vit
 for label in VITALS:
     vital_type = vitals[k]
     print('Current vital being trained:', vital_type)
-    X_train_task3 = all_training_data.pivot_table(index="pid", columns="Time", values=vital_type)
-    print('X_train', X_train_task3)
-    y_train_task3 = all_training_data.pivot_table(index="pid", values=label)
-    print('y_train', y_train_task3)
+    X_train_task3 = train_features.pivot_table(index="pid", columns="Time", values=vital_type).to_numpy()
+    #print('X_train', X_train_task3)
+    y_train_task3 = train_features.pivot_table(index="pid", values=label).to_numpy()
+    #print('y_train', y_train_task3)
     svregression_task3_[str(vital_type)] = svregression_task3.fit(X_train_task3,y_train_task3.ravel())
     k += 1
 
+######################################
+#Task 3 TESTING
+pred_t3=[]
+result_t3 = []
+k = 0 #integer increment at each iterator of the for loop to iterate on the "vitals" list
+result_t3 = result_t2
+
+for label in VITALS:
+    vital_type = vitals[k]
+    
+    X_test_task3 = test_features.pivot_table(index="pid", columns="Time", values=vital_type).to_numpy()
+    
+    pred_t3 = svregression_task3_[str(vital_type)].predict(X_test_task3)
+    result_t3 = np.concatenate((result_t3, np.expand_dims(pred_t3, axis=1)), axis=1)
+    k += 1
+###########################################
+print("result_t3", result_t3.shape)
 ############################################################################
-#TESTING --> To do. The following code is the old version, using numpy instead on pandas for the data manipulation.
 
-pid_attribute_t1 = []
-pid_attribute_t2 = []
-pid_attribute_t3 = []
-
-i=0
-result_final =[]
-for i in range(np.size(pid_list,0)):
-    j=0
-    for j in range(np.size(task_1,0)):
-        while pid_list[i] == train_feature[j,0]:
-            pid_attribute_t1 = np.vstack((pid_attribute_t1, test_feature[j,:]))
-            pid_attribute_t2 = np.concatenate((pid_attribute_t2, test_feature[j,:]), axis=0)
-            pid_attribute_t3 = np.concatenate((pid_attribute_t3, test_feature[j,:]), axis=0)
-            j = j + 1
-
-    task1_pred=svclassifier_task1.predict(pid_attribute_t1)
-    task2_pred=svclassifier_task2.predict(pid_attribute_t2)
-    task3_pred = svregression_task3.predict(pid_attribute_t3)
-    pid_attribute_t1 = []
-    pid_attribute_t2 = []
-    pid_attribute_t3 = []
-
-    result = np.concatenate((task1_pred, task2_pred, task3_pred), axis=1)
-    result_final = np.concatenate((result_final, result), axis=0)
-
-result_final = np.concatenate((pid_list_test, result_final), axis=1)
-DF = pd.DataFrame(result_final)
-DF.to_csv("sumbission.csv", index=False, header=False)
+final_result = np.concatenate((np.expand_dims(head, axis=0), result_t3), axis=0)
+DF = pd.DataFrame(final_result)
+DF.to_csv("sumbission.csv", index=False, header=False, float_format='%.3f')
