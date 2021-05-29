3497429831566e82adc51cb5972419ef63dabc53
Chang Mei-Chih <37042542+MeiChihChang@users.noreply.github.com>
2021-05-29 12:26:15 +0200

    Add files via upload

--- task4/main_mei.py
+++ task4/main_mei.py
@@ -1,3 +1,4 @@
+import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -27,6 +28,7 @@ def preprocess_image(filename):
     image = tf.image.resize(image, target_shape)
     return image
 
+
 def preprocess_triplets(anchor, positive, negative):
     """
     Given the filenames corresponding to the three images, load and
@@ -38,6 +40,58 @@ def preprocess_triplets(anchor, positive, negative):
         preprocess_image(positive),
         preprocess_image(negative),
     )
+
+
+"""
+Let's setup our data pipeline using a zipped list with an anchor, positive,
+and negative image filename as the source. The output of the pipeline
+contains the same triplet with every image loaded and preprocessed.
+"""
+
+imgs_path =  os.getcwd() + "\\food\\" 
+
+anchor_images = []
+positive_images = []
+negative_images = []
+
+train_triplets = open(os.getcwd()  + '\\train_triplets.txt', 'r')
+Lines = train_triplets.readlines()
+
+# Strips the newline character
+for line in Lines:
+    names = line.split()
+    anchor_images.append(os.getcwd() + '\\food\\' + names[0] + ".jpg")
+    positive_images.append(os.getcwd() + '\\food\\' + names[1]+ ".jpg")
+    negative_images.append(os.getcwd() + '\\food\\' + names[2]+ ".jpg") 
+
+image_count = len(anchor_images)
+print(image_count)
+
+anchor_dataset = tf.data.Dataset.from_tensor_slices(anchor_images)
+positive_dataset = tf.data.Dataset.from_tensor_slices(positive_images)
+
+negative_dataset = tf.data.Dataset.from_tensor_slices(negative_images)
+
+dataset = tf.data.Dataset.zip((anchor_dataset, positive_dataset, negative_dataset))
+dataset = dataset.shuffle(buffer_size=1024)
+dataset = dataset.map(preprocess_triplets)
+
+# Let's now split our dataset in train and validation.
+train_dataset = dataset.take(round(image_count * 0.8))
+val_dataset = dataset.skip(round(image_count * 0.8))
+
+train_dataset = train_dataset.batch(32, drop_remainder=False)
+train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+
+val_dataset = val_dataset.batch(32, drop_remainder=False)
+val_dataset = val_dataset.prefetch(tf.data.AUTOTUNE)
+
+
+"""
+Let's take a look at a few examples of triplets. Notice how the first two images
+look alike while the third one is always different.
+"""
+
 
 def visualize(anchor, positive, negative):
     """Visualize a few triplets from the supplied batches."""
@@ -53,7 +107,55 @@ def visualize(anchor, positive, negative):
     for i in range(3):
         show(axs[i, 0], anchor[i])
         show(axs[i, 1], positive[i])
-        show(axs[i, 2], negative[i])    
+        show(axs[i, 2], negative[i])
+
+
+#visualize(*list(train_dataset.take(1).as_numpy_iterator())[0])
+
+"""
+## Setting up the embedding generator model
+Our Siamese Network will generate embeddings for each of the images of the
+triplet. To do this, we will use a ResNet50 model pretrained on ImageNet and
+connect a few `Dense` layers to it so we can learn to separate these
+embeddings.
+We will freeze the weights of all the layers of the model up until the layer `conv5_block1_out`.
+This is important to avoid affecting the weights that the model has already learned.
+We are going to leave the bottom few layers trainable, so that we can fine-tune their weights
+during training.
+"""
+
+base_cnn = resnet.ResNet50(
+    weights="imagenet", input_shape=target_shape + (3,), include_top=False
+)
+
+flatten = layers.Flatten()(base_cnn.output)
+dense1 = layers.Dense(512, activation="relu")(flatten)
+#dense1 = layers.Dense(128, activation="relu")(flatten)
+dense1 = layers.BatchNormalization()(dense1)
+dense2 = layers.Dense(256, activation="relu")(dense1)
+#dense2 = layers.Dense(64, activation="relu")(dense1)
+dense2 = layers.BatchNormalization()(dense2)
+output = layers.Dense(256)(dense2)
+#output = layers.Dense(64)(dense2)
+
+embedding = Model(base_cnn.input, output, name="Embedding")
+
+trainable = False
+for layer in base_cnn.layers:
+    if layer.name == "conv5_block1_out":
+        trainable = True
+    layer.trainable = trainable
+
+"""
+## Setting up the Siamese Network model
+The Siamese network will receive each of the triplet images as an input,
+generate the embeddings, and output the distance between the anchor and the
+positive embedding, as well as the distance between the anchor and the negative
+embedding.
+To compute the distance, we use a custom layer `DistanceLayer` that
+returns both values as a tuple.
+"""
+
 
 class DistanceLayer(layers.Layer):
     """
@@ -68,14 +170,33 @@ class DistanceLayer(layers.Layer):
     def call(self, anchor, positive, negative):
         ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
         an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
-        return (ap_distance, an_distance)  
+        return (ap_distance, an_distance)
+
+
+anchor_input = layers.Input(name="anchor", shape=target_shape + (3,))
+positive_input = layers.Input(name="positive", shape=target_shape + (3,))
+negative_input = layers.Input(name="negative", shape=target_shape + (3,))
+
+distances = DistanceLayer()(
+    embedding(resnet.preprocess_input(anchor_input)),
+    embedding(resnet.preprocess_input(positive_input)),
+    embedding(resnet.preprocess_input(negative_input)),
+)
+
+siamese_network = Model(
+    inputs=[anchor_input, positive_input, negative_input], outputs=distances
+)
+
+"""
+## Putting everything together
+
+"""
+
 
 class SiameseModel(Model):
     """The Siamese Network model with a custom training and testing loops.
-
     Computes the triplet loss using the three embeddings produced by the
     Siamese Network.
-
     The triplet loss is defined as:
        L(A, P, N) = max(‖f(A) - f(P)‖² - ‖f(A) - f(N)‖² + margin, 0)
     """
@@ -133,82 +254,78 @@ class SiameseModel(Model):
     def metrics(self):
         # We need to list our metrics here so the `reset_states()` can be
         # called automatically.
-        return [self.loss_tracker]              
+        return [self.loss_tracker]
 
-imgs_path =  os.getcwd() + "\\food\\" 
-print(imgs_path)
 
-anchor_images = []
-positive_images = []
-negative_images = []
+"""
+## Training
+We are now ready to train our model.
+"""
 
-train_triplets = open(os.getcwd()  + '\\train_triplets.txt', 'r')
-Lines = train_triplets.readlines()
+siamese_model = SiameseModel(siamese_network)
+print(train_dataset.element_spec)
+siamese_model.compile(optimizer=optimizers.Adam(0.0001))
+siamese_model.fit(train_dataset, epochs=10, validation_data=val_dataset)
+
+"""
+## Prediction
+
+"""
+
+test_anchor_images = []
+test_positive_images = []
+test_negative_images = []
+
+test_triplets = open(os.getcwd()  + '\\test_triplets.txt', 'r')
+Lines = test_triplets.readlines()
 
 # Strips the newline character
 for line in Lines:
     names = line.split()
-    anchor_images.append(os.getcwd() + '\\food\\' + names[0] + ".jpg")
-    positive_images.append(os.getcwd() + '\\food\\' + names[1]+ ".jpg")
-    negative_images.append(os.getcwd() + '\\food\\' + names[2]+ ".jpg")  
+    test_anchor_images.append(os.getcwd() + '\\food\\' + names[0] + ".jpg")
+    test_positive_images.append(os.getcwd() + '\\food\\' + names[1]+ ".jpg")
+    test_negative_images.append(os.getcwd() + '\\food\\' + names[2]+ ".jpg") 
 
-image_count = len(anchor_images)
+image_count = len(test_anchor_images)
+print(image_count)
 
-anchor_dataset = tf.data.Dataset.from_tensor_slices(anchor_images)
-positive_dataset = tf.data.Dataset.from_tensor_slices(positive_images)
-negative_dataset = tf.data.Dataset.from_tensor_slices(negative_images)
+test_anchor_dataset = tf.data.Dataset.from_tensor_slices(test_anchor_images)
+test_positive_dataset = tf.data.Dataset.from_tensor_slices(test_positive_images)
+test_negative_dataset = tf.data.Dataset.from_tensor_slices(test_negative_images)
 
-dataset = tf.data.Dataset.zip((anchor_dataset, positive_dataset, negative_dataset))
-dataset = dataset.shuffle(buffer_size=1024)
-dataset = dataset.map(preprocess_triplets)
+dataset2 = tf.data.Dataset.zip((test_anchor_dataset, test_positive_dataset, test_negative_dataset))
+dataset2 = dataset2.shuffle(buffer_size=1024)
+dataset2 = dataset2.map(preprocess_triplets)
+#test_dataset = dataset2.take(image_count)
+test_dataset = dataset2.batch(32, drop_remainder=False)
+test_dataset = test_dataset.prefetch(tf.data.AUTOTUNE)
 
-# Let's now split our dataset in train and validation.
-train_dataset = dataset.take(round(image_count * 0.8))
-val_dataset = dataset.skip(round(image_count * 0.8))
+predictions = []
+count=0
 
-train_dataset = train_dataset.batch(32, drop_remainder=False)
-train_dataset = train_dataset.prefetch(8)
+while count < image_count:
+    count = count + 1
+    sample = next(iter(test_dataset))
 
-val_dataset = val_dataset.batch(32, drop_remainder=False)
-val_dataset = val_dataset.prefetch(8)
+    anchor, positive, negative = sample
+    anchor_embedding, positive_embedding, negative_embedding = (
+        embedding(resnet.preprocess_input(anchor)),
+        embedding(resnet.preprocess_input(positive)),
+        embedding(resnet.preprocess_input(negative)),
+    )
 
-#visualize(*list(train_dataset.take(1).as_numpy_iterator())[0])
+    cosine_similarity = metrics.CosineSimilarity()
 
-base_cnn = resnet.ResNet50(
-    weights="imagenet", input_shape=target_shape + (3,), include_top=False
-)
+    positive_similarity = cosine_similarity(anchor_embedding, positive_embedding)
+    print("Positive similarity:", positive_similarity.numpy())
 
-flatten = layers.Flatten()(base_cnn.output)
-dense1 = layers.Dense(512, activation="relu")(flatten)
-dense1 = layers.BatchNormalization()(dense1)
-dense2 = layers.Dense(256, activation="relu")(dense1)
-dense2 = layers.BatchNormalization()(dense2)
-output = layers.Dense(256)(dense2)
+    negative_similarity = cosine_similarity(anchor_embedding, negative_embedding)
+    print("Negative similarity", negative_similarity.numpy())
 
-embedding = Model(base_cnn.input, output, name="Embedding")
+    if positive_similarity.numpy() > negative_similarity.numpy():
+        predictions.append(1)
+    else:
+        predictions.append(0)
 
-trainable = False
-for layer in base_cnn.layers:
-    if layer.name == "conv5_block1_out":
-        trainable = True
-    layer.trainable = trainable
-
-anchor_input = layers.Input(name="anchor", shape=target_shape + (3,))
-positive_input = layers.Input(name="positive", shape=target_shape + (3,))
-negative_input = layers.Input(name="negative", shape=target_shape + (3,))
-
-distances = DistanceLayer()(
-    embedding(resnet.preprocess_input(anchor_input)),
-    embedding(resnet.preprocess_input(positive_input)),
-    embedding(resnet.preprocess_input(negative_input)),
-)
-
-siamese_network = Model(
-    inputs=[anchor_input, positive_input, negative_input], outputs=distances
-)    
-
-siamese_model = SiameseModel(siamese_network)
-#siamese_model.compile(optimizer=optimizers.Adam(0.0001))
-siamese_model.compile(loss="binary_crossentropy", optimizer="adam",
-	metrics=["accuracy"])
-siamese_model.fit(train_dataset, epochs=10, validation_data=val_dataset)
+DF = pd.DataFrame(predictions)
+DF.to_csv('submission.csv', index=False, header=False)
